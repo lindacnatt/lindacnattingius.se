fdaf6cd221d97f419c182d8bfe7c5dc5fa890a64
lindacnatt <linda.cnattingius@gmail.com>
2020-09-17 19:59:45 +0200

    add graphiql

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,19 @@
 import React from "react"
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 const Footer = () => {
+    const data = useStaticQuery(graphql`
+    query {
+        site {
+            siteMetadata {
+                author
+            }
+        }  
+    }
+`)
     return (
         <footer>
-            <p>By Linda Cnattingius</p>
+            <p>{data.site.siteMetadata.author}</p>
         </footer>
     )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,21 @@
 import React from "react"
-import { Link } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import headerStyles from './header.module.scss'
 
 const Header = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    author
+                }
+            }  
+        }
+    `)
     return (
         <div className={headerStyles.navbar}>
-            <Link className={headerStyles.name} to="/">Linda Cnattingius</Link>
+            <Link className={headerStyles.name} to="/">{data.site.siteMetadata.author}</Link>
             <div>
                 <Link className={headerStyles.link} activeClassName={headerStyles.activeNavItem} to="../about">About</Link>
                 <Link className={headerStyles.link} activeClassName={headerStyles.activeNavItem} to="../projects">Projects</Link>
