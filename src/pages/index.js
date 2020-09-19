8218f2091a8d53c7ebe8edc430d3f6222752886d
lindacnatt <linda.cnattingius@gmail.com>
2020-09-19 19:32:47 +0200

    turn markdown to html

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,40 @@
 import React from "react"
-import {Link} from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 
 
 
 
 const FirstPage = () => {
-  return(
+  const data = useStaticQuery(graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+  `)
+  return (
     <Layout>
       <h1>Hello</h1>
       <h2>I'm Linda Cnattingius</h2>
+      <ol>
+        {data.allMarkdownRemark.edges.map((edge) => {
+          return (
+            <li>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+
+      </ol>
     </Layout>
   )
 }
