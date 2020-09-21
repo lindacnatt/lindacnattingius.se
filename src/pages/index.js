1351e9467303fbaee73ba1ce0cec5b5eafc7d29c
lindacnatt <linda.cnattingius@gmail.com>
2020-09-21 17:52:05 +0200

    graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,11 @@
 import React from "react"
-import { graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 
-
-
-
 const FirstPage = () => {
-  const data = useStaticQuery(graphql`
-  query {
-    allMarkdownRemark {
-      edges {
-        node {
-          frontmatter {
-            title
-            date
-          }
-        }
-      }
-    }
-  }
-  `)
-  return (
+  return(
     <Layout>
-      <h1>Hello</h1>
-      <h2>I'm Linda Cnattingius</h2>
-      <ol>
-        {data.allMarkdownRemark.edges.map((edge) => {
-          return (
-            <li>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
-            </li>
-          )
-        })}
-
-      </ol>
+      <h1>I'm Linda</h1>
+      <p>ping</p>
     </Layout>
   )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,43 @@
 import React from "react"
+
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 
 const ProjectsPage = () => {
+    const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            fields{
+                slug
+            }
+          }
+        }
+      }
+    }
+    `)
     return (
         <Layout>
-            <h1>Projects</h1>
+            <h1>Hello</h1>
+            <h2>I'm Linda Cnattingius</h2>
+            <ol>
+                {data.allMarkdownRemark.edges.map((edge) => {
+                    return (
+                        <li>
+                            <Link to ={`/projects/${edge.node.fields.slug}`}>
+                                <h2>{edge.node.frontmatter.title}</h2>
+                                <p>{edge.node.frontmatter.date}</p>
+                            </Link>
+                        </li>
+                    )
+                })}
+
+            </ol>
         </Layout>
     )
 }
