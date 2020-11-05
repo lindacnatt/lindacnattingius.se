97076a2d944a3c7d3487bb6f1604eb2da17d02d3
lindacnatt <linda.cnattingius@gmail.com>
2020-11-05 21:36:26 +0100

    quick fix for missing 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from '../components/layout'
+
+const ErrorPage = () => {
+    return (
+        <Layout>
+            <h1>404 - Ooops something weird happened</h1>
+            <h2>While you are still here, read some extra info about me ;)</h2> 
+            <p>I like long walks in the forest, and spending time with my loved ones</p>
+        </Layout>
+    )
+}
+export default ErrorPage
