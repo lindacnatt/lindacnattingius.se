c5d83477b006aee1e9777aa8dab9ebc30e19e758
lindacnatt <linda.cnattingius@gmail.com>
2021-04-17 22:32:33 +0200

    start fade on scroll

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,30 @@
-import React from "react"
+import React, { useState }from "react"
 import Header from '../components/header'
 import Footer from '../components/footer'
 import '../styles/index.scss'
 import layoutStyles from './layout.module.scss'
 
 const Layout = (props) => {
+    const [scroll, setScroll] = useState(false);
+    const changeBackground = () => {
+     if(window.scrollY >= 400){
+       setScroll(true)
+     }else{
+       setScroll(false)
+     }
+    };
+
+    window.addEventListener('scroll', changeBackground)
+
     return (
-        <div className={layoutStyles.container}>
-            <div className={layoutStyles.content}>
-                <Header />
-                {props.children}
+        <div className={ scroll ? 'html active' : 'html'}>
+            <div className={layoutStyles.container}>
+                <div className={layoutStyles.content}>
+                    <Header />
+                    {props.children}
+                </div>
+                <Footer />
             </div>
-            <Footer />
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from '../components/layout'
 import Projects from './projects'
 import Hero from '../components/hero'
+import IndexStyles from './index.module.scss'
 
 const FirstPage = () => {
+
+
   return(
-    <Layout>
+    <Layout >
       <Hero/>
       <Projects></Projects>
     </Layout>
