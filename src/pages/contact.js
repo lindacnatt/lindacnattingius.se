4e42eaa1c8d7ed93e4c6dcd935e14e69d37b2855
lindacnatt <linda.cnattingius@gmail.com>
2021-04-18 12:30:54 +0200

    smol things

---
+++ src/pages/contact.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from '../components/layout'
+import Img from 'gatsby-image'
+import pb from '../images/profilbild.png'
+import AboutStyles from './about.module.scss'
+
+const ContactPage = () => {
+    return (
+        <Layout>
+            <div className={AboutStyles.content}>
+                <div className={AboutStyles.text}>
+                    <h1>About me</h1>
+                    <p className={AboutStyles.description}>I'm a developer in love with visual and graphic design</p>
+                </div>
+                <img className={AboutStyles.pb} src={pb} />
+            </div>
+        </Layout>
+    )
+}
+export default ContactPage
