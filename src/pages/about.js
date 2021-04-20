f3a7e1e7c19a4ba67b4b724e7fef8f7e1ebf5867
lindacnatt <linda.cnattingius@gmail.com>
2021-04-20 13:56:13 +0200

    fix stuff

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ const AboutPage = () => {
                     <p>
                     I have gained my skills and experiences from my education, 
                     but also from collaborations with KTH’s student union magazine Osqledaren and from my two years as a Graphic Designer at the consulting company Netlight. 
-                    These skills include: **Web development with Vue and React, Illustration and Animation using the Adobe Suite, collaboration and communication within teams and with clients, and UX practices such as usability testing, iterative processes and user-centered design. </p>
+                    These skills include: Web development with Vue and React, Illustration and Animation using the Adobe Suite, collaboration and communication within teams and with clients, and UX practices such as usability testing, iterative processes and user-centered design. </p>
                     </p>
                     <p>If you think I would be a great fit for your team, feel free to contact me via <a href = "mailto: lindacn@kth.se">email</a> or on <a href="https://www.linkedin.com/in/linda-cnattingius">Linkedin</a>, and let's discuss more!</p>
                     <Link className={AboutStyles.link} to="/"><p> ⟵ Go back</p></Link>
